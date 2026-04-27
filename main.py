a08ed5401bc4e23f0e5ea9af3e4fa4646aea8762
AgostonPali <agoston.palko10@gmail.com>
2026-04-27 23:37:39 +0200

    map structure

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Darts szimuláció inicializálása...")
+    # Ide kerül a fizikai motor és a grafikus felület indítása
+    # valamint a fő ciklus (game loop)
+
+if __name__ == "__main__":
+    main()
